c1c12d703411f493f43b00512610ec9292df0948
A-coder152 <abdullahalsaleem19@gmail.com>
2025-08-30 06:34:15 -0400

    nerf godot face

--- obj.gd
+++ obj.gd
@@ -3,12 +3,14 @@ extends Sprite2D
 @export var rect: Rect2
 var item
 var cells_covered
+var godot_face = preload("res://icon.svg")
 
 func setup(part):
 	item = part
 	var dumb = Vector2(item.blocks.x - 1, item.blocks.y - 1)
 	texture = item.image
-	scale = Vector2(item.blocks.x * 0.375, item.blocks.y * 0.375) + dumb * 0.0375
+	scale = Vector2(item.blocks.x * 1.5, item.blocks.y * 1.5) + dumb * 0.15
+	if texture == godot_face: scale /= 4.0
 	print(scale)
 	rect.size = Vector2(item.blocks.x * 20, item.blocks.y * 20) + dumb * 2
 
